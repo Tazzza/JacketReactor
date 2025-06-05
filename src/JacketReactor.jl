a9e7eaae3cf60b3b13d41abb74302ccb2fd35bda
Tazzza <zcectsv@ucl.ac.uk>
2025-06-05 15:10:23 +0100

    Added packages

---
+++ src/JacketReactor.jl
@@ -0,0 +1,6 @@
+# Import packages
+using DifferentialEquations # For solving the system of ODEs
+using Plots                 # For plotting
+using Fresa                 # For optimisation
+using Statistics            # For ordering arrays of objective function values
+using Printf                # Saving and formatting output
